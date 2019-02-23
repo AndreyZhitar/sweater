efb4148c615ce8bc45f07efafbf06965d07f5d1b
Andrey <AndreyZhitar1106@gmail.com>
2019-02-23 20:25:57 +0200

    Add bean validation and BCryptoPasswordEncoding

--- src/main/java/net/zhitar/sweater/domain/Message.java
+++ src/main/java/net/zhitar/sweater/domain/Message.java
@@ -1,6 +1,9 @@
 package net.zhitar.sweater.domain;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Message {
@@ -8,8 +11,11 @@ public class Message {
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
 
+    @NotBlank(message = "Please fill the message")
+    @Length(max = 2048, message = "Message to long")
     private String text;
 
+    @Length(max = 255, message = "Tag to long")
     private String tag;
 
     @ManyToOne(fetch = FetchType.EAGER)
